a154c6c6d6de79cb73c53e305dff082fa03a284f
anttomi <tomi.salo@myy.haaga-helia.fi>
2021-04-20 17:31:37 +0300

    clr fix

--- src/main/java/com/example/musiclibrary/MusiclibraryApplication.java
+++ src/main/java/com/example/musiclibrary/MusiclibraryApplication.java
@@ -24,14 +24,13 @@ public class MusiclibraryApplication {
 	    return (args) -> {
 		Playlist p1 = new Playlist("ekalista");
 		Playlist p2 = new Playlist("toinenlista");
+		pRepo.deleteAll();
 		pRepo.save(p1);
 		pRepo.save(p2);
-		Song song1 = new Song("In My Time of Need", "Opeth", "Damnation", 2003, "Progressive rock", p1);
-		Song song2 = new Song("Kontinuerlig drift", "Opeth", "In Cauda Venenum", 2019, "Progressive rock", p2);
-		sRepo.save(song1);
-		sRepo.save(song2);
+
 		User user1 = new User("user", "$2a$10$U0021easazMrjG1E2VfKe.ptEgHYSW6RuGTnJFPFAUrBHTXaJUNwC", "USER", "user@user.com");
 		User user2 = new User("admin", "$2a$10$LWRNHeTjf/5FryyyIsUbKezOH1qqqOfJ45smIBjMcpeYP.FmWHscC", "ADMIN", "admin@admin.com");
+		uRep.deleteAll();
 		uRep.save(user1);
 		uRep.save(user2);
 	    };
--- src/main/java/com/example/musiclibrary/WebSecurityConfig.java
+++ src/main/java/com/example/musiclibrary/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 	http
 	.authorizeRequests()
-		.antMatchers("/", "/login", "/songs", "/playlist", "/playlistsongs").permitAll()
+		.antMatchers("/", "/login", "/songs", "/playlists", "/playlistsongs").permitAll()
 		.anyRequest().authenticated()
 		.and()
 	.formLogin()
